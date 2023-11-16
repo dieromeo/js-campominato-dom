55e060f345e930473d167fef67e503f15faf7d6b
dieromeo <diegoromeo99@icloud.com>
2023-11-16 16:06:34 +0100

    feat: aggiunta bombe e punteggio

--- js/main.js
+++ js/main.js
@@ -19,7 +19,21 @@ function createNcells(htmlElement, num, className) { // funzione che crea n elem
         const cell = myCreateDomElement('div', className, i);
         htmlElement.append(cell);
         cell.addEventListener('click', function () { // aggiunge evento click sulle celle
-            clicked(cell, 'yellow');
+            if (valoriBombe.includes(parseInt(cell.innerHTML))) {
+                console.log('sono una bomba');
+                cell.classList.add('red');
+            }
+
+            else if (!alreadyClicked.includes(parseInt(cell.innerHTML))) {
+                alreadyClicked.push(parseInt(cell.innerHTML));
+                console.log(alreadyClicked);
+                clicked(cell, 'yellow');
+                contatore++;
+                console.log(contatore);
+            }
+
+            punteggioUtente.classList.remove('inactive');
+            punteggioUtente.textContent = `Il tuo punteggio è: ${contatore}`;
         })
     }
 }
@@ -32,12 +46,21 @@ function clicked(htmlElement, tag) {  // funzione che aggiunge una classe e stam
 function createByDifficulty(difficulty) {  // funzione che richiama ceateNcells e a seconda della difficoltà inserità ne stampa un numero determinato
     if (difficulty === 'Easy') {
         createNcells(board, 100, 'cell-10');
+        randomArray(100);
+        valoriBombe = randomArray(100);
+        console.log(valoriBombe);
     }
     else if (difficulty === 'Normal') {
         createNcells(board, 81, 'cell-9');
+        randomArray(81);
+        valoriBombe = randomArray(81);
+        console.log(valoriBombe);
     }
     else if (difficulty === 'Hard') {
         createNcells(board, 49, 'cell-7');
+        randomArray(49);
+        valoriBombe = randomArray(49);
+        console.log(valoriBombe);
     }
 }
 
@@ -58,6 +81,16 @@ function campoMinato() {
     }
 }
 
+function randomArray(difficulty) {
+    const arrayCasuale = [];
+    while (arrayCasuale.length < 16) {
+        const arrayElement = Math.floor(Math.random() * difficulty + 1);
+        if (!arrayCasuale.includes(arrayElement)) {
+            arrayCasuale.push(arrayElement);
+        }
+    }
+    return arrayCasuale;
+}
 
 
 
@@ -68,6 +101,11 @@ const boardContainer = document.querySelector('.board-container'); // variabile 
 let state = true; // variabile di stato
 let board;
 const infoUtente = document.getElementById('info'); // h2 da nascondere al click
+let valoriBombe;
+const alreadyClicked = [];
+let contatore = 0;
+const punteggioUtente = document.getElementById('punteggio');
+
 
 
 startButton.addEventListener('click', campoMinato);
